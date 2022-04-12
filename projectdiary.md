412db765828c43fad77dac36b6974af84dd0c533
Jacqueline <jhjzhou@gmail.com>
2022-04-12 09:04:09 +0200

    additional css styling

--- projectdiary.md
+++ projectdiary.md
@@ -3,6 +3,7 @@ Day 0: playing tetris, wireframing, and pseudocode
     I first made the wireframe for the HTML structure of the game, then I played a few different versions of Tetris online (for research purposes!). I outlined the features I wanted to replicate, and the basic functionality of the pieces that I would need to account for. Then, I started outlining what variables, events, intervals, and functions I would need and what they would need to be able to do (and not do).
     The functions that stumped me the most at this point was figuring out how to rotate pieces, how to drop one active piece at a time, and how to clear a line and shift all pieces above it down a line.
 
+
 Day 1: start of coding
     Today I submitted my wireframe and pseudocode for review, and once approved I jumped into coding. Not going to lie, was pretty excited to finally crack into the plan I had outlined!
     By the end of the day, I added in the grids in the game, began putting together the interval to drop pieces, and added in a function to move pieces horizontally when the left/right arrows are pressed and down when the down arrow is pressed.
@@ -21,4 +22,80 @@ Day 4:
     Additionally, I added in a pause button (realized it would be an easy feature to implement, and also found it helpful to be able to pause the game so I could take a look at the console at a specific moment in time).
 
 Day 5:
-    The deadline is tomorrow, and I have all 
+    
+
+
+
+Pre-Project Start Notes:
+TODO: Notes
+When the user clicks start, an interval needs to begin running to start dropping shapes in the grid. Shapes will drop from the top center
+In order to do that, we need to randomly pick a shape, then we need it to drop down line by line every few seconds
+User should be able to move it side by side (w/ left + right keys) or speed the drop down (w/ down key).
+User should also be able to rotate the shape as it falls down, but shape needs to be able to remain in the grid and also continue falling from the last position
+Once a shape hits the bottom or touches a placed shape, we need a new shape to drop down
+When the bottom line is fully filled, we need it to clear the row and move all grid items down one row, also increasing points and lines completed
+after a certain score or lines complete is reached, we can increase the level and increase the speed of the interval
+If the player is too slow and the shapes reach the top, the game should automatically end
+
+
+TODO: Variables
+//* playerScore - tracks score of user
+//* playerLevel - tracks level of user
+//* playerLines - tracks # of completed lines
+//* highScore - tracks highest local score
+//* width - width of grid (10)
+//* height - height of grid (20)
+//* currentShape - current tetrimino shape
+//* nextShape - next tetrimino shape
+//* intervalSpeed - speed of shape drop, decreasing as user levels up
+//* movementY = how many rows we've moved downwards
+//* movementX = how many columsn we've moved left or right
+
+Elements
+//* tetrisGrid = querySelect the grid div so we can make the grid
+
+
+TODO: Functions
+
+Interval: checks if there is an active shape in the grid,
+   if no (ex. start of game), drop a shape at top. Shapes always start at the same positions, so we can 
+   if yes, check if shape is touching another shape or if shape has reached the bottom of the screen OR if there is a shape in the first row of the grid
+       if touching another shape/reached bottom, leave the shape and start dropping another shape
+       if there is a shape currently in first row, end the game
+       if no to both, remove current shape then return it one row lower
+
+
+generateShape:
+   random number to pick a tetrimino shape for current and next shape
+
+moveShape:
+   if right arrow, moves shape right 1, if left arrow clicked, moves shape left 1. if down arrow clicked, moves shape down 1
+
+rotateElement:
+   needs to rotate shape 90deg clockwise....
+   removes current shape and replaces it with rotated shape (will have set dimensions, which will add vertical movement and add/minus horizontal movement)
+
+clearLine:
+   when a line is completely filled, remove line and shift all elements above it down
+
+gameOver:
+   when blocks reach the top and there's no space left, end the game
+
+
+TODO: Events
+click:
+  start button -> starts the game
+  replay button -> replay the game
+
+keydown:
+//*    up -> rotate element via roateElement function
+//*    left -> move shape to left
+//*    right -> move shape to right
+//*    down -> move shape down one line
+
+? Extra Features - if there's time
+play sound effect when a line is cleared
+sound effect when player levels up/speed increases
+play/pause background music button
+column shading indicating where the shape will drop
+save a shape that can be swapped with current shape later (probably no time??)
--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,7 @@ function init() {
   function createGrid(grid, cellCount, cellArray) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
-      // cell.innerHTML = i
+      cell.innerHTML = i
       grid.appendChild(cell)
       cell.dataset.index = i
       cellArray.push(cell)
@@ -28,10 +28,10 @@ function init() {
 
   const audioLineClear = new Audio('./media/clear-line.wav')
   const audioGameOver = new Audio('./media/game-over.wav')
-  const audioLevelUp = new Audio('./level-up-1.wav')
+  const audioLevelUp = new Audio('./media/level-up-1.wav')
   const audioNoMovement = new Audio('./media/no-movement.wav')
-  const audioMovement = new Audio('./media/movement.wav')
-  const audioRotate = new Audio('./rotate-shape.wav')
+  const audioRotate = new Audio('./media/movement.wav')
+  const audioMovement = new Audio('./media/rotateshape.wav')
 
   // ! Game Functionality
 
@@ -41,7 +41,7 @@ function init() {
   pauseButton.addEventListener('click', gamePause)
   window.addEventListener('keydown', handleKeyDown)
 
-  const highScore = document.querySelector('#highscore')
+  let highScore = document.querySelector('#highscore')
   let gameOn = false
   let gameOver = false
   let playerScore = 0
@@ -66,7 +66,7 @@ function init() {
       //   rotationArray.map(index => index + mainWidth * 3)],
       // this.startingRotationPosition = this.currentRotationPosition,
       this.currentRotationPosition = [3, 4, 5, 6],
-      this.startingRotationPosition = [3, 4, 5, 6],
+      this.startingRotationPosition = rotationArray,
       this.rotationState = 0,
       this.nextPosition = []
     }
@@ -132,6 +132,7 @@ function init() {
         audioNoMovement.play()
         clearLine()
         currentShape = nextShape
+        xChange = 0
       } else { // if both conditions above are false, lower shape by one row
         removeTetromino()
         currentShape.currentPosition = currentShape.nextPosition
@@ -146,9 +147,9 @@ function init() {
       nextShape.currentPosition = nextShape.startingPosition
       previewNext()
       if (currentShape.startingPosition.some(index => mainCells[index].className.includes('paused'))) {
+        audioGameOver.play()
         gamePause()
         gameOver = true
-        audioGameOver.play()
         window.alert('Game over!')
         currentShape.currentPosition.forEach(index => {
           mainCells[index].classList.add(currentShape.active)
@@ -190,7 +191,15 @@ function init() {
   }
 
   function rotateTetromino() {
-    console.log('hello')
+    audioRotate.play()
+    let testArray = []
+    const yChange = Math.floor(currentShape.currentPosition[0] / mainWidth)
+    testArray.push(currentShape.startingRotationPosition.map(index => index + mainWidth + (yChange * mainWidth)),
+      currentShape.startingRotationPosition.map(index => index + mainWidth * 2),
+      currentShape.startingRotationPosition.map(index => index + mainWidth * 3),
+      currentShape.startingRotationPosition.map(index => index + mainWidth * 4))
+    console.log(testArray)
+    
   }
 
   // function rotateTetromino() {
@@ -301,80 +310,5 @@ function init() {
   createGrid(nextGrid, nextCellCount, nextCells)
 
 }
-
-// TODO: Notes
-// When the user clicks start, an interval needs to begin running to start dropping shapes in the grid. Shapes will drop from the top center
-// In order to do that, we need to randomly pick a shape, then we need it to drop down line by line every few seconds
-// User should be able to move it side by side (w/ left + right keys) or speed the drop down (w/ down key).
-// User should also be able to rotate the shape as it falls down, but shape needs to be able to remain in the grid and also continue falling from the last position
-// Once a shape hits the bottom or touches a placed shape, we need a new shape to drop down
-// When the bottom line is fully filled, we need it to clear the row and move all grid items down one row, also increasing points and lines completed
-// after a certain score or lines complete is reached, we can increase the level and increase the speed of the interval
-// If the player is too slow and the shapes reach the top, the game should automatically end
-
-
-// TODO: Variables
-//* playerScore - tracks score of user
-//* playerLevel - tracks level of user
-//* playerLines - tracks # of completed lines
-//* highScore - tracks highest local score
-//* width - width of grid (10)
-//* height - height of grid (20)
-//* currentShape - current tetrimino shape
-//* nextShape - next tetrimino shape
-//* intervalSpeed - speed of shape drop, decreasing as user levels up
-//* movementY = how many rows we've moved downwards
-//* movementX = how many columsn we've moved left or right
-
-// Elements
-//* tetrisGrid = querySelect the grid div so we can make the grid
-
-
-// TODO: Functions
-
-// Interval: checks if there is an active shape in the grid,
-//    if no (ex. start of game), drop a shape at top. Shapes always start at the same positions, so we can 
-//    if yes, check if shape is touching another shape or if shape has reached the bottom of the screen OR if there is a shape in the first row of the grid
-//        if touching another shape/reached bottom, leave the shape and start dropping another shape
-//        if there is a shape currently in first row, end the game
-//        if no to both, remove current shape then return it one row lower
-
-
-// generateShape:
-//    random number to pick a tetrimino shape for current and next shape
-
-// moveShape:
-//    if right arrow, moves shape right 1, if left arrow clicked, moves shape left 1. if down arrow clicked, moves shape down 1
-
-// rotateElement:
-//    needs to rotate shape 90deg clockwise....
-//    removes current shape and replaces it with rotated shape (will have set dimensions, which will add vertical movement and add/minus horizontal movement)
-
-// clearLine:
-//    when a line is completely filled, remove line and shift all elements above it down
-
-// gameOver:
-//    when blocks reach the top and there's no space left, end the game
-
-
-// TODO: Events
-// click:
-//*    start button -> starts the game
-//    replay button -> replay the game
-
-// keydown:
-//*    up -> rotate element via roateElement function
-//*    left -> move shape to left
-//*    right -> move shape to right
-//*    down -> move shape down one line
-
-// ? Extra Features - if there's time
-// play sound effect when a line is cleared
-// sound effect when player levels up/speed increases
-// play/pause background music button
-// column shading indicating where the shape will drop
-// save a shape that can be swapped with current shape later (probably no time??)
-
-
 
 window.addEventListener('DOMContentLoaded', init)
